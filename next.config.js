0e8abbf45eb22118476d8ab564f1fb988479d645
dfitfactor <dee@dfitfactor.com>
2026-03-23 21:46:51 -0400

    Add auth alias redirects for custom domain

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ const nextConfig = {
       allowedOrigins: ['localhost:3000', 'localhost:3001'],
     },
   },
+  async redirects() {
+    return [
+      {
+        source: '/login',
+        destination: '/auth/login',
+        permanent: false,
+      },
+      {
+        source: '/create-account',
+        destination: '/auth/signup',
+        permanent: false,
+      },
+    ]
+  },
   webpack: (config, { dev }) => {
     if (dev) {
       // Avoid filesystem cache issues on Windows (missing *.pack.gz files).
